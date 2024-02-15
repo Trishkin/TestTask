a0d8e0da0238a9bfef91988683613db8bd23eac9
“Kulikov <dima.dmitry.kulikov@gmail.com>
2024-02-15 14:49:38 +0300

    Change 2

--- TestTask/Data/ApplicationDbContext.cs
+++ TestTask/Data/ApplicationDbContext.cs
@@ -12,6 +12,7 @@ namespace TestTask.Data
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
+            Database.EnsureCreated();
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
